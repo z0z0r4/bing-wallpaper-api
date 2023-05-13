cb2ba10d1c36ad0df537cba8d780f7877c3fb913
z0z0r4 <z0z0r4@outlook.com>
2023-05-13 12:49:39 +0800

    fix abslote path

--- crawler.sh
+++ crawler.sh
@@ -2,4 +2,5 @@ export PYENV_ROOT="/root/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 source <(pyenv init -)
 python --version
+cd /opt/bing-wallpaper-api
 python crawler.py
